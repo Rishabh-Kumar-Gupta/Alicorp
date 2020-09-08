366ad05410403045629ae3b25b78a960d3a0f467
rishabh.gupta@sigmoidanalytics.com <rishabh.gupta@sigmoidanalytics.com>
2020-09-08 10:49:08 -0700

    Update

--- appleCovid/test.py
+++ appleCovid/test.py
@@ -1,10 +1,11 @@
 import pytest
 
 from driver import AllDataCSV
-
+from main import urlFinder
 
 def fix():
-    csv_url = "https://covid19-static.cdn-apple.com/covid19-mobility-data/2016HotfixDev8/v3/en-us/applemobilitytrends-2020-09-05.csv"
+    # csv_url = "https://covid19-static.cdn-apple.com/covid19-mobility-data/2016HotfixDev8/v3/en-us/applemobilitytrends-2020-09-05.csv"
+    csv_url = urlFinder()
     nameOfDownloadedFile = 'downloaded.csv'
     nameOfSavingFile = "save.csv"
     obj = AllDataCSV(csv_url, nameOfDownloadedFile, nameOfSavingFile)
